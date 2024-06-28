76e33d3d5ab004558a0fe4ec3e85be071ef32d7b
Sasanka Ravindu <61648299+SasankaRW@users.noreply.github.com>
2024-06-28 22:34:41 +0530

    agenda implement

--- src/app/components/agenda.tsx
+++ src/app/components/agenda.tsx
@@ -1,8 +1,39 @@
+"use client";
+
+import { useEffect, useState } from 'react';
+
 const Agenda = () => {
+  const [content, setContent] = useState('');
+
+  useEffect(() => {
+    fetch("https://sessionize.com/api/v2/qainm5td/view/GridSmart")
+      .then(response => response.text())
+      .then(data => {
+        setContent(data);
+      })
+      .catch(error => {
+        console.error('Error fetching the session data:', error);
+      });
+
+    // Clean up script when component unmounts
+    return () => {
+      setContent('');
+    };
+  }, []);
+
   return (
-    <section className="container max-w-8xl h-screen flex flex-col items-center justify-center">
-      <h1 className="text-6xl font-bold">Agenda</h1>
-    </section>
+
+    <div>
+      <section className="container max-w-8xl h-screen flex flex-col items-center justify-center">
+      {/* Placeholder while loading */}
+      {content ? (
+        <div dangerouslySetInnerHTML={{ __html: content }} />
+      ) : (
+        <p>Loading agenda...</p>
+      )}
+      </section>
+      
+    </div>
   );
 };
 
