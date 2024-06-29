976b6493f31419e2e0c742e7a517a62a57eed518
Bawanthathilan <bawantharathnayaka@gmail.com>
2024-06-29 09:50:19 +0530

    fixed mobile responsive issues in landing page

--- src/components/home/Keynote.tsx
+++ src/components/home/Keynote.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 
 const Keynote = () => {
   return (
-    <div className="flex flex-col items-center px-32 mt-10">
-      <div className="flex flex-row justify-between gap-5">
+    <div className="container mx-auto flex flex-col items-center mt-10">
+      <div className="flex flex-col mobile:flex-row justify-between gap-5">
         <VideoCard
           image="/io24-featured-keynote-recap-videos.webp"
           title="Watch the recap videos"
@@ -23,7 +23,7 @@ const Keynote = () => {
         />
       </div>
 
-      <div className="mt-10 flex flex-row justify-between w-full gap-10">
+      <div className="mt-10 flex flex-col mobile:flex-row justify-between w-full gap-10">
         <div className="relative flex-1 flex flex-row border-[1px] border-white rounded-2xl lg:flex-row overflow-hidden justify-between">
           <div className="flex flex-col items-start p-10 pr-0">
             <span className="text-grey dark:text-white mb-3 text-md:mb-4 sm:s-h4 md:l-h4 text-5xl">
@@ -37,7 +37,7 @@ const Keynote = () => {
               </p>
               <Link
                 href="#"
-                className="px-4 py-3 rounded-full border-white border-[1px]"
+                className="px-4 py-3 rounded-full border-white border-[1px] hover:bg-white hover:text-black"
               >
                 Get Started
               </Link>
@@ -65,7 +65,7 @@ const Keynote = () => {
               </p>
               <Link
                 href="#"
-                className="px-4 py-3 rounded-full border-white border-[1px]"
+                className="px-4 py-3 rounded-full border-white border-[1px] hover:bg-white hover:text-black"
               >
                 Get Started
               </Link>
