9575f4d1481be43481a50b4df20088a1b97537df
chamodshehanka <hcsperera@gmail.com>
2024-06-29 14:55:18 +0530

    Stats updated

--- src/components/home/stats.tsx
+++ src/components/home/stats.tsx
@@ -5,7 +5,7 @@ const Stats = () => {
   return (
     <div className="container mt-10 rounded-2xl border-[1px] border-border px-5 py-10 flex flex-col md:flex-row justify-between items-center">
       <div className="flex flex-col gap-5 w-full md:w-2/5">
-        <h2 className="text-5xl font-normal">Occuring in the near future</h2>
+        <h2 className="text-5xl font-normal">Happening on 20th July, 2024</h2>
         <p className="font-thin text-sm">
           Google I/O Extended brings the excitement of Google I/O directly to
           community. Join I/O Extended for the latest developer updates,
@@ -14,9 +14,9 @@ const Stats = () => {
       </div>
       <div className="grid grid-cols-2 md:flex md:flex-row gap-5 mt-5 md:mt-0">
         <ImageCard title="Speakers" no={5}/>
-        <ImageCard title="Participants" no={100}/>
+        <ImageCard title="Participants" no={1000}/>
         <ImageCard title="Sessions" no={5}/>
-        <ImageCard title="Workshops" no={1}/>
+        <ImageCard title="Panel Discussions" no={1}/>
       </div>
     </div>
   );
