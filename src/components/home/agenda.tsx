30b2e00991663835aac0d0ecb578df77b840d2bc
chamodshehanka <hcsperera@gmail.com>
2024-07-01 19:54:16 +0530

    Chore: skelton added agenda adding

--- src/components/home/agenda.tsx
+++ src/components/home/agenda.tsx
@@ -1,7 +1,30 @@
+"use client";
+
+import { Skeleton } from "@/components/ui/skeleton";
+import { useGetAgendaGrid } from "@/hooks/useAgendaGrid";
+
 const Agenda = () => {
+  const { data: agendaData, isLoading } = useGetAgendaGrid();
+
   return (
-    <section className="container max-w-8xl h-screen flex flex-col items-center justify-center">
-      <h1 className="text-6xl font-bold">Agenda</h1>
+    <section className="container max-w-8xl h-[90vh] flex flex-col items-center justify-center">
+      <h1 className="text-6xl font-bold mb-10">Agenda</h1>
+
+      {isLoading && <Skeleton className="h-12 w-12 rounded-full" />}
+
+      <div className={"mt-15"}>
+        {!isLoading &&
+          agendaData?.[0]?.rooms[0]?.sessions?.map((session: any) => (
+            <div
+              key={session?.id}
+              className={
+                "text-white text-2xl border-2 border-blue-500 bg-blue-700 p-2 m-2 rounded-3xl"
+              }
+            >
+              <p className={"ml-4"}>{session.title}</p>
+            </div>
+          ))}
+      </div>
     </section>
   );
 };
