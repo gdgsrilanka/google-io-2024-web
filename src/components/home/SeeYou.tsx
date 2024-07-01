2ae86d4b2a870fde7f3fa7bb7a1029f5d6270867
heshanthaka <heshanthaka007@gmail.com>
2024-07-01 23:36:18 +0530

    fix: update styling on see you section

--- src/components/home/SeeYou.tsx
+++ src/components/home/SeeYou.tsx
@@ -3,17 +3,15 @@ import Image from "next/image";
 
 const SeeYou = () => {
   return (
-    <div className="mt-28">
-      <div className="flex flex-col items-start px-10">
-        <h1 className="title text-5xl font-medium">See you at the event!</h1>
-      </div>
-
-      <div className="relative w-full mobile:h-[100px] md:h-[420px]">
-        <Image
-          src="io24-see-you-next-year-dark.svg"
-          fill
-          alt="io24-see-you-next-year"
-        />
+    <div className="mt-28 relative w-full h-[420px] md2:h-[220px]">
+      <Image
+        src="io24-see-you-next-year-dark.svg"
+        fill
+        alt="io24-see-you-next-year"
+        className="object-contain"
+      />
+      <div className="absolute md:top-10 md2:top-0 left-0 px-5 sm:px-10 py-5">
+        <h1 className="title text-xl sm:text-3xl md:text-4xl lg:text-5xl font-medium text-white">See you at the event!</h1>
       </div>
     </div>
   );
